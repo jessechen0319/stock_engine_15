df2c266b07a0cfa97d247599f30e934b5164b928
xiang19890319@gmail.com <xiang19890319@gmail.com>
2017-12-02 21:17:07 +0800

    add Boll Raise

--- routes/server/AnalysisCenter.js
+++ routes/server/AnalysisCenter.js
@@ -1,4 +1,5 @@
 let raiseFromFlor = require('./calculators/底部突破');
+let bollRaise = require('./calculators/突破布林线');
 
 let calculators=[raiseFromFlor.calculate];
 
@@ -7,10 +8,45 @@ function analysis(data, stock){
     
     if(data.length>150){
         _generateFibla(data);
-        calculators.forEach((calculator)=>{
-            raiseFromFlor.calculate(data, stock);
-        });
+        _generateBoll(data);
+        // calculators.forEach((calculator)=>{
+        //     raiseFromFlor.calculate(data, stock);
+        // });
+        bollRaise.calculate(data, stock);
     }
+}
+
+function _generateBoll(data){
+    // MA（n）=n天均价
+    // n=周期
+    // c=收盘价
+    // P=BOLL带宽度，一般为2
+    // 设c-MA=D
+    // 先计算标准差
+    // 设标准差为MD
+    // MD=(【D^2+前一天的D^2+...+（n-1)天的D^2】/n)再开平方
+    // 上轨=MA+P*MD
+    // 下轨=MA-p*MD
+    for(let i = data.length-20; i<data.length; i++){
+        let ma_n = 0;
+        for(let j = i-19; j<=i; j++){
+            ma_n = ma_n + Number(data[j].close);
+        }
+        ma_n = ma_n/20;
+        let md = 0;
+        for(let j = i-19; j<=i; j++){
+            let squire = Number(data[j].close) - ma_n;
+            squire = squire*squire;
+            md = md + squire;
+        }
+        md = md/20;
+        md = Math.sqrt(md);
+        data[i].Boll_ma = ma_n;
+        data[i].Boll_up = ma_n + 2*md;
+        data[i].Boll_down = ma_n - 2*md;
+    }
+
+    return data;
 }
 
 function _generateFibla(data){
---
+++ routes/server/calculators/突破布林线.js
@@ -0,0 +1,52 @@
+//4根均线有34和55是向上的，股价突破Boll线中规
+//此前8天价格都在Boll线中轨下方
+
+let SendResult = require('./popupResult');
+//strategy name=底部突破
+function calculate(data, stock){
+    if(data.length < 155){
+        return false;
+    }
+    let latestRecord = data[data.length-1];
+    let compare = data[data.length-2];
+
+    if(latestRecord.m34 >= compare.m34){
+        if(latestRecord.m55 >= compare.m55){
+            let isBiggerThanBoll = false;
+            for(let i = date.length - 9; i<= data.length-2; i++){
+                if(data[i].close>data[i].Boll_ma){
+                    isBiggerThanBoll = true;
+                }
+            }
+
+            if(!isBiggerThanBoll){
+                if(latestRecord.close>latestRecord.Boll_ma){
+                    if(latestRecord.volume>compare.volume){
+                        SendResult.sendResultToServer(stock, 'stg_2', latestRecord.day);
+                    }
+                }
+            }
+        }
+    }
+
+    // if(latestRecord.m13 > compare.m13 && latestRecord.m34 >= compare.m34){//13, 34上攻
+    //     let cha13 = Math.abs(latestRecord.m13 - latestRecord.m55)/latestRecord.open;
+    //     let cha34 = Math.abs(latestRecord.m34 - latestRecord.m55)/latestRecord.open;
+    //     if(cha13<0.01 && cha34<0.01){//均线粘合
+    //         let cha144 = latestRecord.m144 - data[data.length-8].m144;
+    //         cha144 = Math.abs(cha144);
+    //         if(cha144<=0.03){//144走平
+    //             let cha = latestRecord.m144 - latestRecord.close;
+    //             cha = cha/latestRecord.close;
+    //             if(cha>0.025){//144高过两个点
+    //                 if(latestRecord.close>latestRecord.m13 && latestRecord.close>latestRecord.m34 && latestRecord.close>latestRecord.m55){
+    //                     //股价站上所有均线
+    //                     SendResult.sendResultToServer(stock, 'stg_1', latestRecord.day);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+}
+
+exports.calculate = calculate;
